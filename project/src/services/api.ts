4cc5695028d859c453ec75c56565457116b215ee
AshishNith <ranjan.ashish9992@gmail.com>
2025-08-04 21:09:56 +0530

    Update

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
 
 export interface Initiative {
   _id?: string;
