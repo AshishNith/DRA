4cb8e11b8ad29ad338e26de17f77a8ee211d9674
AshishNith <ranjan.ashish9992@gmail.com>
2025-08-04 23:03:25 +0530

    Updte

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://dra-phi.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
