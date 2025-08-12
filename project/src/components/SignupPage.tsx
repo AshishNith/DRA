edcb22a305e3a58b532315d8d4a256fba13269f8
AshishNith <ranjan.ashish9992@gmail.com>
2025-08-12 14:37:58 +0530

    responsove

--- project/src/components/SignupPage.tsx
+++ project/src/components/SignupPage.tsx
@@ -1,4 +1,4 @@
- q    import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import "../styles/Signup.css";
 import { useAuth } from '../auth/AuthContext';
